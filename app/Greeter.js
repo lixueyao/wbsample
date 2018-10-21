d6b2ec8927d14c5dae76681d5c07594919b6558a
“lixueyaochn” <lixueyaochn@gmail.com>
2018-10-21 22:51:37 +0800

    first commit

---
+++ app/Greeter.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import config from './config.json';
+import styles from './Greeter.css';//导入
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div className={styles.root}> //使用cssModule添加类名的方法
+	  	<div className={styles.text}>text</div>
+        {config.greetText}
+      </div>
+    );
+  }
+}
+
+export default Greeter
---
+++ app/main.js
@@ -0,0 +1,8 @@
+import babelenv from 'babel-preset-env';
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+import './main.css';
+
+render(<Greeter />, document.getElementById('root'));
